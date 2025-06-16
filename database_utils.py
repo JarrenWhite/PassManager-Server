bc05491157bccc0a3b85a4d97bb2c5f76866a4db
JarrenWhite <phillrucci@hotmail.co.uk>
2025-06-16 21:16:59 +0100

    Added function to delete existing users.

--- database_utils.py
+++ database_utils.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from database_setup import User
+from database_setup import User, Session as UserSession, PasswordItem
 
 
 def register_user(session: Session, username: str, hashed_password: str)  -> User | None:
@@ -16,3 +16,14 @@ def register_user(session: Session, username: str, hashed_password: str)  -> Use
 
 def get_user(session: Session, username: str):
     return session.query(User).filter_by(username=username).first()
+
+def delete_user(session: Session, username: str):
+    user = session.query(User).filter_by(username=username).first()
+    if not user:
+        return False
+    
+    session.query(UserSession).filter_by(user_id=user.id).delete()
+    session.query(PasswordItem).filter_by(user_id=user.id).delete()
+    session.delete(user)
+    session.commit()
+    return True
