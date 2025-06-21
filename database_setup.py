6c96b8163844493699649d8aa1bfc389540e23a3
JarrenWhite <phillrucci@hotmail.co.uk>
2025-06-21 11:18:27 +0100

    Refactor database setup to use non-global variables.

--- database_setup.py
+++ database_setup.py
@@ -3,13 +3,19 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
 
-DB_FILENAME = os.getenv("VAULT_PATH", "data/vault.db")
-DB_URL = f"sqlite:///{DB_FILENAME}"
+def get_db_filename():
+    return os.getenv("VAULT_PATH", "data/vault.db")
 
-engine = create_engine(DB_URL, pool_pre_ping=True)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+def get_db_url():
+    return f"sqlite:///" + get_db_filename()
+
+def get_engine():
+    return create_engine(get_db_url(), pool_pre_ping=True)
+
+def get_session_local():
+    return sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+
 Base = declarative_base()
-
 
 class User(Base):
     __tablename__ = "user"
@@ -41,10 +47,11 @@ class Encrypted(Base):
 
 
 def init_db():
-    if not os.path.exists(DB_FILENAME):
+    db_filename = get_db_filename()
+    if not os.path.exists(db_filename):
         print("Database not found. Creating new database...")
-        os.makedirs("data", exist_ok=True)
-        engine = create_engine(DB_URL)
+        os.makedirs(os.path.dirname(db_filename), exist_ok=True)
+        engine = get_engine()
         Base.metadata.create_all(engine)
         print("Database created.")
 
