f1bcde320a7929c745034057a96db7cf5a03844b
JarrenWhite <phillrucci@hotmail.co.uk>
2025-06-26 20:55:42 +0100

    Start of refactor to codebase to better match python coding standards.

--- database/database_models.py
+++ database/database_models.py
@@ -1,19 +1,5 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
-
-def get_db_filename():
-    return os.getenv("VAULT_PATH", "data/vault.db")
-
-def get_db_url():
-    return f"sqlite:///" + get_db_filename()
-
-def get_engine():
-    return create_engine(get_db_url(), pool_pre_ping=True)
-
-def get_session_local():
-    return sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -23,7 +9,7 @@ class User(Base):
     username = Column(String, unique=True, nullable=False)
     password_hash = Column(String, nullable=False)
 
-class Session(Base):
+class LoginSession(Base):
     __tablename__ = "session"
     id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey("user.id"), nullable=False)
@@ -43,18 +29,4 @@ class Encrypted(Base):
     id = Column(Integer, primary_key=True)
     username = Column(String)
     password = Column(String)
-    notes = Column(String)
-
-
-def init_db():
-    db_filename = get_db_filename()
-    if not os.path.exists(db_filename):
-        print("Database not found. Creating new database...")
-        os.makedirs(os.path.dirname(db_filename), exist_ok=True)
-        engine = get_engine()
-        Base.metadata.create_all(engine)
-        print("Database created.")
-
-if __name__ == "__main__":
-    print("Running database_setup.py as __main__.")
-    init_db()
+    notes = Column(String) 
---
+++ database/database_setup.py
@@ -0,0 +1,27 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from .database_models import Base
+
+def get_db_filename():
+    return os.getenv("VAULT_PATH", "data/vault.db")
+
+def get_db_url():
+    return f"sqlite:///{get_db_filename()}"
+
+def get_engine():
+    return create_engine(get_db_url(), pool_pre_ping=True)
+
+def get_session_local():
+    return sessionmaker(autocommit=False, autoflush=False, bind=get_engine())
+
+def init_db():
+    db_filename = get_db_filename()
+    if not os.path.exists(db_filename):
+        print("Database not found. Creating new database...")
+        os.makedirs(os.path.dirname(db_filename), exist_ok=True)
+        engine = get_engine()
+        Base.metadata.create_all(engine)
+        print("Database created.")
