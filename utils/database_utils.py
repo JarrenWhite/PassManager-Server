e7de6bf4dcb20eca7223845febd05df73b5521ec
JarrenWhite <phillrucci@hotmail.co.uk>
2025-07-08 22:14:32 +0100

    Several further database utils functions completed.

--- utils/database_utils.py
+++ utils/database_utils.py
@@ -52,7 +52,7 @@ class DatabaseUtils:
                 return True
 
         except Exception as e:
-            logger.error(f"create_user: Error '{username}': {e}")
+            logger.error(f"create_user: Error - {e}")
             return False
 
     @staticmethod
@@ -64,7 +64,7 @@ class DatabaseUtils:
                 return user.password_hash if user else None
 
         except Exception as e:
-            logger.error(f"get_user_password_hash: Error retrieving user '{username}': {e}")
+            logger.error(f"get_user_password_hash: Error - {e}")
             return None
 
     @staticmethod
@@ -83,7 +83,7 @@ class DatabaseUtils:
                 return True
 
         except Exception as e:
-            logger.error(f"delete_user: Error '{username}': {e}")
+            logger.error(f"delete_user: Error - {e}")
             return False
 
     @staticmethod
@@ -97,12 +97,13 @@ class DatabaseUtils:
                     logger.warning(f"create_session: User '{username}' could not be found.")
                     return False
 
-                # Create login session for them
+                # Create login session
                 expiry = datetime.now() + timedelta(duration_till_expiry)
                 new_login_session = LoginSession(user=user, token=token, expiry=expiry)
 
                 session.add(new_login_session)
                 logger.info(f"create_session: User '{username}' login session created.")
+                return True
 
         except Exception as e:
             logger.error(f"create_session: Error '{username}': {e}")
@@ -123,7 +124,7 @@ class DatabaseUtils:
                     return user.username
 
         except Exception as e:
-            logger.error(f"check_session_token: Error '{token[:-4]}': {e}")
+            logger.error(f"check_session_token: Error - {e}")
             return
 
     @staticmethod
@@ -135,24 +136,56 @@ class DatabaseUtils:
                 login_session = session.scalar(select(LoginSession).where(LoginSession.token == token))
                 if not login_session:
                     logger.warning(f"delete_session: Token '{token[:-4]}' could not be found.")
-                    return
+                    return False
 
                 session.delete(login_session)
                 logger.info(f"delete_session: Token '{token[:-4]}' deleted successfully.")
+                return True
 
         except Exception as e:
-            logger.error(f"delete_session: Error '{token[:-4]}': {e}")
-            return
+            logger.error(f"delete_session: Error - {e}")
+            return False
 
     @staticmethod
     def delete_all_sessions(username: str) -> bool:
         """Delete all sessions for the given user"""
-        pass
+        try:
+            with DatabaseUtils.get_db_session() as session:
+                # Find user in question
+                user = session.scalar(select(User).where(User.username == username))
+                if not user:
+                    logger.warning(f"delete_all_sessions: User '{username}' could not be found.")
+                    return False
+
+                all_login_sessions = user.login_sessions
+                for login_session in all_login_sessions:
+                    session.delete(login_session)
+                logger.info(f"delete_all_sessions: All login sessions for '{username}' deleted.")
+                return True
+
+        except Exception as e:
+            logger.error(f"delete_all_sessions: Error - {e}")
+            return False
 
     @staticmethod
-    def clean_sessions():
+    def clean_sessions() -> bool:
         """Remove all sessions with past expiry dates"""
-        pass
+        try:
+            with DatabaseUtils.get_db_session() as session:
+                current_time = datetime.now()
+                expired_sessions = session.scalars(
+                    select(LoginSession).where(LoginSession.expiry < current_time)
+                ).all()
+
+                for session_obj in expired_sessions:
+                    session.delete(session_obj)
+
+                logger.info(f"clean_sessions: Deleted {len(expired_sessions)} sessions.")
+                return True
+
+        except Exception as e:
+            logger.error(f"clean_sessions: Error - {e}")
+            return False
 
     @staticmethod
     def create_secure_data(username: str,
@@ -162,7 +195,29 @@ class DatabaseUtils:
                            password_enc: str,
                            notes_enc: str) -> bool:
         """Create an instance of secure data"""
-        pass
+        try:
+            with DatabaseUtils.get_db_session() as session:
+                # Find user in question
+                user = session.scalar(select(User).where(User.username == username))
+                if not user:
+                    logger.warning(f"create_secure_data: User '{username}' could not be found.")
+                    return False
+
+                # Create secure data
+                new_secure_data = SecureData(user=user,
+                                             entry_name=entry_name,
+                                             website=website_enc,
+                                             username=username_enc,
+                                             password=password_enc,
+                                             notes=notes_enc)
+
+                session.add(new_secure_data)
+                logger.info(f"create_secure_data: Secure data '{new_secure_data.entry_public_id}' created successfully.")
+                return True
+
+        except Exception as e:
+            logger.error(f"create_secure_data: Error - {e}")
+            return False
 
     @staticmethod
     def edit_secure_data(entry_public_id: str,
@@ -172,7 +227,32 @@ class DatabaseUtils:
                          password_enc: Optional[str],
                          notes_enc: Optional[str]) -> bool:
         """Edit an existing instance of secure data"""
-        pass
+        try:
+            with DatabaseUtils.get_db_session() as session:
+                # Find secure data in question
+                secure_data = session.scalar(select(SecureData).where(SecureData.public_id == entry_public_id))
+                if not secure_data:
+                    logger.warning(f"edit_secure_data: User '{entry_public_id}' could not be found.")
+                    return False
+
+                # Edit secure data
+                if entry_name:
+                    secure_data.entry_name = entry_name
+                if website_enc:
+                    secure_data.website = website_enc
+                if username_enc:
+                    secure_data.username = username_enc
+                if password_enc:
+                    secure_data.password = password_enc
+                if notes_enc:
+                    secure_data.notes = notes_enc
+
+                logger.info(f"edit_secure_data: Secure data '{new_secure_data.entry_public_id}' edited successfully.")
+                return True
+
+        except Exception as e:
+            logger.error(f"edit_secure_data: Error - {e}")
+            return False
 
     @staticmethod
     def delete_secure_data(entry_public_id: str):
