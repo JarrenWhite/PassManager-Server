cae5c42f3684e4544c5b793d8ecc00ee8d6a7f27
JarrenWhite <phillrucci@hotmail.co.uk>
2025-07-22 18:16:47 +0100

    Outline sanitisation functions, and add them to user services.

--- services/user_service.py
+++ services/user_service.py
@@ -8,9 +8,19 @@ from utils import Sanitise
 def user_register(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
     """Register a user - business logic"""
     required_keys = {"username", "password"}
-    ok, error = Sanitise.verify_keys(data, required_keys)
+    ok, error = Sanitise.keys(data, required_keys)
     if not ok:
         logger.warning("user_register: Rejected - Incorrect keys.")
+        return error, 400
+
+    ok, error = Sanitise.username(data["username"])
+    if not ok:
+        logger.warning("user_register: Rejected - Username issue.")
+        return error, 400
+
+    ok, error = Sanitise.password(data["password"])
+    if not ok:
+        logger.warning("user_register: Rejected - Password issue.")
         return error, 400
 
     return {}, 201
@@ -18,9 +28,19 @@ def user_register(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
 def user_auth(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
     """Authorize a user - business logic"""
     required_keys = {"username", "password"}
-    ok, error = Sanitise.verify_keys(data, required_keys)
+    ok, error = Sanitise.keys(data, required_keys)
     if not ok:
         logger.warning("user_auth: Rejected - Incorrect keys.")
+        return error, 400
+
+    ok, error = Sanitise.username(data["username"])
+    if not ok:
+        logger.warning("user_auth: Rejected - Username issue.")
+        return error, 400
+
+    ok, error = Sanitise.password(data["password"])
+    if not ok:
+        logger.warning("user_auth: Rejected - Password issue.")
         return error, 400
 
     return {}, 201
@@ -28,9 +48,19 @@ def user_auth(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
 def user_delete(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
     """Delete a user - business logic"""
     required_keys = {"username", "password"}
-    ok, error = Sanitise.verify_keys(data, required_keys)
+    ok, error = Sanitise.keys(data, required_keys)
     if not ok:
         logger.warning("user_delete: Rejected - Incorrect keys.")
+        return error, 400
+
+    ok, error = Sanitise.username(data["username"])
+    if not ok:
+        logger.warning("user_delete: Rejected - Username issue.")
+        return error, 400
+
+    ok, error = Sanitise.password(data["password"])
+    if not ok:
+        logger.warning("user_delete: Rejected - Password issue.")
         return error, 400
 
     return {}, 201
--- utils/sanitise_utils.py
+++ utils/sanitise_utils.py
@@ -4,11 +4,21 @@ from typing import Dict, Any, Tuple, Optional
 class Sanitise:
 
     @staticmethod
-    def verify_keys(data: Dict[str, Any], required_keys: set[str]) -> Tuple[bool, Dict[str, Any]]:
+    def keys(data: Dict[str, Any], required_keys: set[str]) -> Tuple[bool, Dict[str, Any]]:
+        """Verify that the keys received are correct"""
         if set(data) != required_keys:
             allowed_list = "', '".join(sorted(required_keys))
             return False, {
                 "error": f"Allowed and required parameters are: '{allowed_list}'"
             }
-        else:
-            return True, {}
+        return True, {}
+
+    @staticmethod
+    def username(username: str) -> Tuple[bool, Dict[str, Any]]:
+        """Sanitise the received username"""
+        return True, {}
+
+    @staticmethod
+    def password(password_hash: str) -> Tuple[bool, Dict[str, Any]]:
+        """Sanitise the received password hash"""
+        return True, {}
