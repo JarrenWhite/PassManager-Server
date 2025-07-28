22a298834607382f4ed2016b958b0e2c632439c8
JarrenWhite <phillrucci@hotmail.co.uk>
2025-07-28 08:57:32 +0100

    Implemented complete_user_registration, to finish creating a user.

--- services/user_service.py
+++ services/user_service.py
@@ -29,20 +29,38 @@ def begin_user_registration() -> Tuple[Dict[str, Any], int]:
 def complete_user_registration(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
     """Register a user - business logic"""
     # Sanitise inputs
-    required_keys = {"username", "secret_key"}
+    required_keys = {"registration_id", "secret_key_plain", "secret_key_enc", "username"}
     ok, error = Service.sanitise_inputs(data, required_keys, "complete_user_registration")
     if not ok:
         return error, 400
 
-    # Attempt data entry
+    registration_id = data["registration_id"]
+    secret_key_plain = data["secret_key_plain"]
+    secret_key_enc = data["secret_key_enc"]
     username = data["username"]
-    secret_key = data["secret_key"]
-    ok, failure_reason = Database.create_user(username, secret_key)
 
-    # Failure
+    # Fetch hashed secret key
+    ok, failure_reason, secret_key_hash = Database.fetch_registeration(registration_id)
     if not ok:
         assert failure_reason is not None
         return Service.handle_failure(failure_reason, "complete_user_registration")
+    assert secret_key_hash is not None
+
+    # Check that received plain key links to stored hash
+    if hashlib.sha256(secret_key_plain).hexdigest() != secret_key_hash:
+        logger.warning("complete_user_registration: Rejected - Secret key hashes do not align.")
+        return {"error": "The received key does not match the stored one for this ID"}, 400
+
+    # Attempt data entry
+    ok, failure_reason = Database.create_user(username, secret_key_hash, secret_key_enc)
+    if not ok:
+        assert failure_reason is not None
+        return Service.handle_failure(failure_reason, "complete_user_registration")
+
+    # Delete registration entry
+    ok, failure_reason = Database.delete_registeration(registration_id)
+    if not ok:
+        logger.error(f"complete_user_registration: Failed to delete registration {registration_id} after user creation.")
 
     # User created
     return {}, 201
