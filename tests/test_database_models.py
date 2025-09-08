6b2758dfea8b76af8a81ff4fdb5c0d4f43375b4b
JarrenWhite <phillrucci@hotmail.co.uk>
2025-09-08 20:35:33 +0100

    First database models test created.

---
+++ tests/test_database_models.py
@@ -0,0 +1,45 @@
+import os
+import sys
+import pytest
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, clear_mappers
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from database.database_models import Base, User
+
+class TestDatabaseUserModels():
+    """Test cases for the User database model"""
+
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self):
+        self.engine = create_engine("sqlite://:memory:")
+        Base.metadata.create_all(self.engine)
+
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+
+        yield
+
+        self.session.close()
+        clear_mappers()
+        Base.metadata.drop_all(self.engine)
+
+    def test_can_create_user(self):
+        """Should be able to create new user"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
+
+        db_user = self.session.query(User).first()
+        assert db_user is not None
+        assert db_user.username_hash == user.username_hash
+
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
