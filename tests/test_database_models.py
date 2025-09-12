8ef8edda334efc29bf51bd647a7d07d3e59c0027
JarrenWhite <phillrucci@hotmail.co.uk>
2025-09-12 16:09:31 +0100

    Add multiple further tests for the database model.

--- tests/test_database_models.py
+++ tests/test_database_models.py
@@ -41,7 +41,6 @@ class TestDatabaseUserModels():
 
     def test_can_use_optional_fields(self):
         """Should be able to create new user with optional fields"""
-
         user = User(
             username_hash="fake_hash",
             srp_salt="fake_srp_salt",
@@ -84,6 +83,160 @@ class TestDatabaseUserModels():
             error_message = str(e).lower()
             assert ("unique constraint failed" in error_message or "integrity" in error_message), f"Expected uniqueness constraint violation, got: {error_message}"
             self.session.rollback()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+
+    def test_all_fields_correct(self):
+        """Should store all fields correctly"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            new_srp_salt="new_srp_salt",
+            new_srp_verifier="new_srp_verifier",
+            new_master_key_salt="new_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+        assert users[0].username_hash == "fake_hash"
+        assert users[0].srp_salt == "fake_srp_salt"
+        assert users[0].srp_verifier == "fake_srp_verifier"
+        assert users[0].master_key_salt == "fake_master_key_salt"
+        assert users[0].new_srp_salt == "new_srp_salt"
+        assert users[0].new_srp_verifier == "new_srp_verifier"
+        assert users[0].new_master_key_salt == "new_master_key_salt"
+
+    def test_can_edit_data(self):
+        """Should be possible to adjust all fields"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+        assert users[0].username_hash == "fake_hash"
+
+        user.username_hash="new_fake_hash"
+        user.srp_salt="new_fake_srp_salt"
+        user.srp_verifier="new_fake_srp_verifier"
+        user.master_key_salt="new_fake_master_key_salt"
+        self.session.commit()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 0
+
+        users = self.session.query(User).filter_by(username_hash="new_fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+        assert users[0].username_hash == "new_fake_hash"
+        assert users[0].srp_salt == "new_fake_srp_salt"
+        assert users[0].srp_verifier == "new_fake_srp_verifier"
+        assert users[0].master_key_salt == "new_fake_master_key_salt"
+
+    def test_can_add_optional_fields_late(self):
+        """Should be able to add optional fields at a later point"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
+
+        user.new_srp_salt="new_srp_salt"
+        user.new_srp_verifier="new_srp_verifier"
+        user.new_master_key_salt="new_master_key_salt"
+        self.session.commit()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+        assert users[0].new_srp_salt == "new_srp_salt"
+        assert users[0].new_srp_verifier == "new_srp_verifier"
+        assert users[0].new_master_key_salt == "new_master_key_salt"
+
+    def test_cannot_delete_required_fields(self):
+        """Should not be possible to delete required fields"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
+
+        user.username_hash=None
+        try:
+            self.session.commit()
+            raise AssertionError("Expected not null constraint violation but no exception was raised")
+        except Exception as e:
+            error_message = str(e).lower()
+            assert ("not null constraint failed" in error_message or "integrity" in error_message), f"Expected not null constraint violation, got: {error_message}"
+            self.session.rollback()
+
+        user.srp_salt=None
+        try:
+            self.session.commit()
+            raise AssertionError("Expected not null constraint violation but no exception was raised")
+        except Exception as e:
+            error_message = str(e).lower()
+            assert ("not null constraint failed" in error_message or "integrity" in error_message), f"Expected not null constraint violation, got: {error_message}"
+            self.session.rollback()
+
+        user.srp_verifier=None
+        try:
+            self.session.commit()
+            raise AssertionError("Expected not null constraint violation but no exception was raised")
+        except Exception as e:
+            error_message = str(e).lower()
+            assert ("not null constraint failed" in error_message or "integrity" in error_message), f"Expected not null constraint violation, got: {error_message}"
+            self.session.rollback()
+
+        user.master_key_salt=None
+        try:
+            self.session.commit()
+            raise AssertionError("Expected not null constraint violation but no exception was raised")
+        except Exception as e:
+            error_message = str(e).lower()
+            assert ("not null constraint failed" in error_message or "integrity" in error_message), f"Expected not null constraint violation, got: {error_message}"
+            self.session.rollback()
+
+        users = self.session.query(User).filter_by(username_hash="fake_hash").all()
+        assert len(users) == 1
+        assert users[0].id == user.id
+        assert users[0].username_hash == "fake_hash"
+        assert users[0].srp_salt == "fake_srp_salt"
+        assert users[0].srp_verifier == "fake_srp_verifier"
+        assert users[0].master_key_salt == "fake_master_key_salt"
+
+    def test_can_delete_completed_optional_fields(self):
+        """Should be possible to delete optional fields"""
+        user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            new_srp_salt="new_srp_salt",
+            new_srp_verifier="new_srp_verifier",
+            new_master_key_salt="new_master_key_salt"
+        )
+        self.session.add(user)
+        self.session.commit()
 
         users = self.session.query(User).filter_by(username_hash="fake_hash").all()
         assert len(users) == 1
