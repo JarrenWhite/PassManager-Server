3daa09cfa7e2d978fea49253aa4bd18c25699c60
JarrenWhite <phillrucci@hotmail.co.uk>
2025-10-02 18:28:13 +0100

    Created test for change username nominal case. Created _FakeQuery class to handle queries.

--- tests/test_utils_database_utils.py
+++ tests/test_utils_database_utils.py
@@ -13,6 +13,17 @@ from database.database_setup import DatabaseSetup
 from database.database_models import User
 
 
+class _FakeQuery:
+    def __init__(self, results):
+        self._results = results
+
+    def filter(self, condition):
+        return self
+
+    def first(self):
+        return self._results[0] if self._results else None
+
+
 class _FakeSession:
     def __init__(self, on_commit: Optional[Callable[[], None]] = None):
         self._added = []
@@ -35,6 +46,9 @@ class _FakeSession:
     def close(self):
         self.closed = True
 
+    def query(self):
+        pass
+
 
 def _prepare_fake_session(test_class, monkeypatch, exception_message : Optional[str] = None):
     """Prepare a Fake Session with the given exception on commit"""
@@ -47,14 +61,22 @@ def _prepare_fake_session(test_class, monkeypatch, exception_message : Optional[
         test_class._fake_session = _FakeSession()
         monkeypatch.setattr(DatabaseSetup, "get_session", lambda: (lambda: test_class._fake_session))
 
-def _prepare_db_not_initialised_error(self, monkeypatch):
+def _prepare_db_not_initialised_error(monkeypatch):
     def _raise_runtime_error():
         raise RuntimeError("Database not initialised.")
     monkeypatch.setattr(DatabaseSetup, "get_session", lambda: _raise_runtime_error)
 
 
+def _fake_query_response(monkeypatch, response_list):
+    def fake_query(self, model):
+        return _FakeQuery(response_list)
+    monkeypatch.setattr(_FakeSession, "query", fake_query)
+
+
 class TestCreateUser():
     """Test cases for the database utilities create_user function"""
+
+    _fake_session: _FakeSession
 
     def test_nominal_case(self, monkeypatch):
         """Should create user and add to Database"""
@@ -73,18 +95,18 @@ class TestCreateUser():
         assert response[1] == None
 
         assert len(self._fake_session._added) == 1
-        created_user = self._fake_session._added[0]
-        assert created_user.username_hash == "fake_hash"
-        assert created_user.srp_salt == "fake_srp_salt"
-        assert created_user.srp_verifier == "fake_srp_verifier"
-        assert created_user.master_key_salt == "fake_master_key_salt"
+        db_user = self._fake_session._added[0]
+        assert db_user.username_hash == "fake_hash"
+        assert db_user.srp_salt == "fake_srp_salt"
+        assert db_user.srp_verifier == "fake_srp_verifier"
+        assert db_user.master_key_salt == "fake_master_key_salt"
         assert self._fake_session.commits == 1
         assert self._fake_session.rollbacks == 0
         assert self._fake_session.closed is True
 
     def test_handles_database_unprepared_failure(self, monkeypatch):
         """Should return correct failure reason if database is not setup"""
-        _prepare_db_not_initialised_error(self, monkeypatch)
+        _prepare_db_not_initialised_error(monkeypatch)
 
         response = DatabaseUtils.create_user(
             username_hash="fake_hash",
@@ -151,6 +173,43 @@ class TestCreateUser():
 
         assert self._fake_session.commits == 1
         assert self._fake_session.rollbacks == 1
+        assert self._fake_session.closed is True
+
+
+class TestChangeUsername():
+    """Test cases for the database utilities change_username function"""
+
+    _fake_session: _FakeSession
+
+    def test_nominal_case(self, monkeypatch):
+        """Should change username of given user"""
+        _prepare_fake_session(self, monkeypatch)
+
+        fake_user = User(
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt"
+        )
+        self._fake_session.add(fake_user)
+
+        _fake_query_response(monkeypatch, [fake_user])
+
+        response = DatabaseUtils.change_username(
+            username_hash="fake_hash",
+            new_username_hash="new_fake_hash"
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert response[0] == True
+        assert response[1] == None
+
+        assert len(self._fake_session._added) == 1
+        db_user = self._fake_session._added[0]
+        assert db_user.username_hash == "new_fake_hash"
+        assert self._fake_session.commits == 1
+        assert self._fake_session.rollbacks == 0
         assert self._fake_session.closed is True
 
 
