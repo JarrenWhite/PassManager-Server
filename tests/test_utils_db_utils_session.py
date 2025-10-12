a32f9b42e18e2bb3ff79b2f956002ca1c26d6245
JarrenWhite <phillrucci@hotmail.co.uk>
2025-10-12 13:48:28 +0100

    Tests for expired login sessions

--- tests/test_utils_db_utils_session.py
+++ tests/test_utils_db_utils_session.py
@@ -865,6 +865,99 @@ class TestCleanUser():
         assert str(condition.left.name) == "id"
         assert condition.right.value == 123456
 
+    def test_deletes_expired_sessions(self, monkeypatch):
+        """Should delete all expired login sessions for given user"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        last_used = datetime.now() - timedelta(hours=1)
+        expiry_time = datetime.now() - timedelta(seconds=1)
+        fake_login_session_one = LoginSession(
+            user_id=123456,
+            public_id="session_fake_public_id_one",
+            session_key="fake_session_key",
+            request_count=3,
+            last_used=last_used,
+            maximum_requests=None,
+            expiry_time=expiry_time,
+            password_change=False
+        )
+        fake_login_session_two = LoginSession(
+            user_id=123456,
+            public_id="session_fake_public_id_two",
+            session_key="fake_session_key",
+            request_count=3,
+            last_used=last_used,
+            maximum_requests=None,
+            expiry_time=expiry_time,
+            password_change=False
+        )
+        expiry_time = datetime.now() + timedelta(seconds=1)
+        fake_login_session_three = LoginSession(
+            user_id=123456,
+            public_id="session_fake_public_id_three",
+            session_key="fake_session_key",
+            request_count=3,
+            last_used=last_used,
+            maximum_requests=None,
+            expiry_time=expiry_time,
+            password_change=False
+        )
+
+        fake_user = User(
+            id=123456,
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            password_change=False,
+            login_sessions=[fake_login_session_one, fake_login_session_two, fake_login_session_three]
+        )
+
+        mock_query = _MockQuery([fake_user])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        response = DBUtilsSession.clean_user(
+            user_id=123456
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert response[0] == True
+        assert response[1] == None
+
+        assert len(mock_session._deletes) == 3
+        deleted_ids = [
+            mock_session._deletes[0].public_id,
+            mock_session._deletes[1].public_id,
+            mock_session._deletes[2].public_id
+        ]
+        assert "session_fake_public_id_one" in deleted_ids
+        assert "session_fake_public_id_two" in deleted_ids
+        assert "session_fake_public_id_three" in deleted_ids
+        assert mock_session.commits == 1
+        assert mock_session.rollbacks == 0
+        assert mock_session.closed is True
+
+        assert len(mock_query._filters) == 1
+        condition = mock_query._filters[0]
+        assert isinstance(condition, BinaryExpression)
+        assert str(condition.left.name) == "id"
+        assert condition.right.value == 123456
+
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
