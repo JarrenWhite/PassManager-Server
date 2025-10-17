80fec565074fd1f0957e2d17e9533f5b23520397
JarrenWhite <phillrucci@hotmail.co.uk>
2025-10-18 00:07:41 +0100

    Test for data entry changes

--- tests/test_utils_db_utils_data.py
+++ tests/test_utils_db_utils_data.py
@@ -290,6 +290,131 @@ class TestEdit():
         assert secure_data.entry_name == "new_fake_entry_name"
         assert secure_data.entry_data == "fake_entry_data"
 
+    def test_change_data(self, monkeypatch):
+        """Should correctly change the entry data, without changing the entry name"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        fake_user = User(
+            id=123456,
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            password_change=False
+        )
+
+        secure_data = SecureData(
+            user=fake_user,
+            public_id="fake_public_id",
+            entry_name="fake_entry_name",
+            entry_data="fake_entry_data"
+        )
+
+        mock_query = _MockQuery([secure_data])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        response = DBUtilsData.edit(
+            public_id="fake_public_id",
+            entry_name=None,
+            entry_data="new_fake_entry_data"
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert response[0] == True
+        assert response[1] == None
+
+        assert len(mock_session._added) == 0
+        assert len(mock_session._deletes) == 0
+        assert mock_session.commits == 1
+        assert mock_session.rollbacks == 0
+        assert mock_session.closed is True
+
+        assert len(mock_query._filters) == 1
+        condition = mock_query._filters[0]
+        assert isinstance(condition, BinaryExpression)
+        assert str(condition.left.name) == "public_id"
+        assert condition.right.value == "fake_public_id"
+
+        assert secure_data.entry_name == "fake_entry_name"
+        assert secure_data.entry_data == "new_fake_entry_data"
+
+    def test_change_both_fields(self, monkeypatch):
+        """Should correctly change both entry name and entry data"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        fake_user = User(
+            id=123456,
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            password_change=False
+        )
+
+        secure_data = SecureData(
+            user=fake_user,
+            public_id="fake_public_id",
+            entry_name="fake_entry_name",
+            entry_data="fake_entry_data"
+        )
+
+        mock_query = _MockQuery([secure_data])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        response = DBUtilsData.edit(
+            public_id="fake_public_id",
+            entry_name="new_fake_entry_name",
+            entry_data="new_fake_entry_data"
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert response[0] == True
+        assert response[1] == None
+
+        assert len(mock_session._added) == 0
+        assert len(mock_session._deletes) == 0
+        assert mock_session.commits == 1
+        assert mock_session.rollbacks == 0
+        assert mock_session.closed is True
+
+        assert len(mock_query._filters) == 1
+        condition = mock_query._filters[0]
+        assert isinstance(condition, BinaryExpression)
+        assert str(condition.left.name) == "public_id"
+        assert condition.right.value == "fake_public_id"
+
+        assert secure_data.entry_name == "new_fake_entry_name"
+        assert secure_data.entry_data == "new_fake_entry_data"
 
 
 if __name__ == '__main__':
