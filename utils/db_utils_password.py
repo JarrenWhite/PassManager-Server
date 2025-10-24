ba0184c70d9ff821ac1bfa63dc4b43a8bca62c37
JarrenWhite <phillrucci@hotmail.co.uk>
2025-10-24 14:45:44 +0100

    Added signature for password.start method.

--- utils/db_utils_password.py
+++ utils/db_utils_password.py
@@ -1,5 +1,11 @@
+from datetime import datetime
+from typing import Tuple, Optional
+
 from sqlalchemy.orm import Session
+
 from database import User
+from .utils_enums import FailureReason
+
 
 class DBUtilsPassword():
     """Utility functions for managing password change based database functions"""
@@ -11,3 +17,21 @@ class DBUtilsPassword():
     ):
         """Remove all partial password change entries, ephemerals and login sessions"""
         pass
+
+    @staticmethod
+    def start(
+        username_hash: str,
+        eph_private_b: str,
+        eph_public_b: str,
+        expiry_time: datetime,
+        srp_salt: str,
+        srp_verifier: str,
+        master_key_salt: str
+    ) -> Tuple[bool, Optional[FailureReason], str]:
+        """
+        Begin password auth ephemeral session for the user
+
+        Returns:
+            (str)   public_id
+        """
+        return False, None, ""
