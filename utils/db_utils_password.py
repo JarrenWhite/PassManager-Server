87202c3c876af14b22fc49c279627495a872d6d2
Jarren White <phillrucci@hotmail.co.uk>
2025-11-02 20:14:00 +0000

    Update password.update to handle already completed secure data

--- utils/db_utils_password.py
+++ utils/db_utils_password.py
@@ -167,6 +167,10 @@ class DBUtilsPassword():
             if secure_data is None:
                 return False, FailureReason.NOT_FOUND
 
+            if secure_data.new_entry_name or secure_data.new_entry_data:
+                DBUtilsPassword.clean_password_change(session, secure_data.user)
+                return False, FailureReason.DUPLICATE
+
             secure_data.new_entry_name = entry_name
             secure_data.new_entry_data = entry_data
 
