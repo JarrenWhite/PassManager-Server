e8b7980bdb234322afa655811db9552dcec056c7
Jarren White <phillrucci@hotmail.co.uk>
2025-11-02 14:34:35 +0000

    Test that password.abort calls clean_password_change for user.

--- tests/test_utils_db_utils_password.py
+++ tests/test_utils_db_utils_password.py
@@ -1478,5 +1478,76 @@ class TestComplete():
         assert mock_session.closed is True
 
 
+class TestAbort():
+    """Test cases for database utils password abort function"""
+
+    def test_nominal_case(self, monkeypatch):
+        """Should fetch the user and call clean_password_change"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        login_session = LoginSession(
+            user_id=123456,
+            public_id="session_fake_public_id",
+            session_key="fake_session_key",
+            request_count=3,
+            last_used=datetime.now() - timedelta(hours=1),
+            maximum_requests=None,
+            expiry_time=datetime.now() + timedelta(hours=1),
+            password_change=True
+        )
+
+        fake_user = User(
+            id=123456,
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            password_change=True,
+            new_srp_salt="new_fake_srp_salt",
+            new_srp_verifier="new_fake_srp_verifier",
+            new_master_key_salt="new_fake_master_key_salt",
+            auth_ephemerals=[],
+            login_sessions=[
+                login_session
+            ],
+            secure_data=[]
+        )
+
+        mock_query = _MockQuery([fake_user])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        called = {"cleaned": False, "user": None}
+        def fake_clean_password(db_session, user):
+            called["cleaned"] = True
+            called["user"] = user
+        monkeypatch.setattr(DBUtilsPassword, "clean_password_change", fake_clean_password)
+
+        response = DBUtilsPassword.abort(
+            user_id=123456
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert response[0] == True
+        assert response[1] == None
+
+        assert called["cleaned"] == True
+        assert called["user"] == fake_user
+
+
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
