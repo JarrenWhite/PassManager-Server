2154806ba8057779170f064ce5365c2995a08bcb
Jarren White <phillrucci@hotmail.co.uk>
2025-11-03 21:31:16 +0000

    data.get_entry update to allow password calls if specifically stated

--- tests/test_utils_db_utils_data.py
+++ tests/test_utils_db_utils_data.py
@@ -1028,6 +1028,67 @@ class TestGetEntry():
         assert response[0] == False
         assert response[1] == FailureReason.PASSWORD_CHANGE
 
+    def test_password_change_fetch(self, monkeypatch):
+        """Should correctly fetch details for password change user if flag is true"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        fake_user = User(
+            id=123456,
+            username_hash="fake_hash",
+            srp_salt="fake_srp_salt",
+            srp_verifier="fake_srp_verifier",
+            master_key_salt="fake_master_key_salt",
+            password_change=False
+        )
+
+        secure_data = SecureData(
+            user=fake_user,
+            public_id="fake_public_id",
+            entry_name="fake_entry_name",
+            entry_data="fake_entry_data"
+        )
+
+        mock_query = _MockQuery([secure_data])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        response = DBUtilsData.get_entry(
+            public_id="fake_public_id",
+            password_change=True
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert isinstance(response[2], str)
+        assert isinstance(response[3], str)
+        assert response[0] == True
+        assert response[1] == None
+        assert response[2] == "fake_entry_name"
+        assert response[3] == "fake_entry_data"
+
+        assert mock_session.commits == 1
+        assert mock_session.rollbacks == 0
+        assert mock_session.closed is True
+
+        assert len(mock_query._filters) == 1
+        condition = mock_query._filters[0]
+        assert isinstance(condition, BinaryExpression)
+        assert str(condition.left.name) == "public_id"
+        assert condition.right.value == "fake_public_id"
+
 
 class TestGetList():
     """Test cases for database utils data get list function"""
