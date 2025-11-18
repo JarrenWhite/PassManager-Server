9b8c15dd231c209a15ed5478016ec39ad9c3fb7d
Jarren White <phillrucci@hotmail.co.uk>
2025-11-18 20:35:34 +0000

    Create class to handle databse config.

---
+++ utils/database_config.py
@@ -0,0 +1,43 @@
+from configparser import ConfigParser
+from typing import Optional
+from pathlib import Path
+
+
+class DatabaseConfig:
+    _config = None
+
+
+    @classmethod
+    def load(cls, file_path: Optional[Path] = None):
+        if cls._config is not None:
+            return
+
+        if not file_path:
+            file_path = Path(__file__).parent.parent / "config" / "db_config.ini"
+
+        parser = ConfigParser()
+        read_files = parser.read(file_path)
+
+        if not read_files:
+            raise FileNotFoundError(f"Config file not found: {file_path}")
+
+        cls._config = parser
+
+
+    @classmethod
+    def get_path(cls, key: str) -> Optional[Path]:
+        if cls._config is None:
+            cls.load()
+
+        section = cls._config.get("paths") # type: ignore
+
+        if section is None:
+            return None
+        value = section.get(key).strip()
+        if not value:
+            return None
+
+        try:
+            return Path(value)
+        except Exception:
+            return None
