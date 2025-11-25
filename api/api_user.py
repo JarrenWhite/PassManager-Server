6474a9e28c9a72c717a7b25f06d0f61c8fcc72ff
JarrenWhite <phillrucci@hotmail.co.uk>
2025-11-25 20:32:26 +0000

    Create user_api front page.

---
+++ api/api_user.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, jsonify
+
+import logging
+logger = logging.getLogger("api")
+
+
+user_bp = Blueprint('user', __name__, url_prefix='/api/user')
+
+
+@user_bp.route('/health', methods=['GET'])
+def health_check():
+    """Health check endpoint for the session API"""
+    logger.info("user health_check: Received")
+    return jsonify({
+        'status': 'healthy',
+        'service': 'data-api'
+    }), 200
