ff13c66aadb220c01c2827f344f58888e277b951
JarrenWhite <phillrucci@hotmail.co.uk>
2025-11-25 20:59:00 +0000

    Create API outlines for User apis.

--- api/api_user.py
+++ api/api_user.py
@@ -7,6 +7,36 @@ logger = logging.getLogger("api")
 user_bp = Blueprint('user', __name__, url_prefix='/api/user')
 
 
+@user_bp.route('/register', methods=['POST'])
+def user_register():
+    """Register a new user"""
+
+    logger.info("Register new user: Called")
+    result, status_code = jsonify({"": ""}), 200
+    logger.info(f"Register new user: Complete with status code: {status_code}")
+    return jsonify(result), status_code
+
+
+@user_bp.route('/username', methods=['POST'])
+def user_username():
+    """Change username"""
+
+    logger.info("Change username: Called")
+    result, status_code = jsonify({"": ""}), 200
+    logger.info(f"Change username: Complete with status code: {status_code}")
+    return jsonify(result), status_code
+
+
+@user_bp.route('/delete', methods=['POST'])
+def user_delete():
+    """Delete user"""
+
+    logger.info("Delete user: Called")
+    result, status_code = jsonify({"": ""}), 200
+    logger.info(f"Delete user: Complete with status code: {status_code}")
+    return jsonify(result), status_code
+
+
 @user_bp.route('/health', methods=['GET'])
 def health_check():
     """Health check endpoint for the user API"""
