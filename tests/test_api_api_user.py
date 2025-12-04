a2da82e24d336031607ad60de3d756c922688c31
JarrenWhite <phillrucci@hotmail.co.uk>
2025-12-04 21:34:09 +0000

    Tests for User Username endpoint.

--- tests/test_api_api_user.py
+++ tests/test_api_api_user.py
@@ -11,7 +11,7 @@ from services.service_user import ServiceUser
 
 
 class TestRegister():
-    """Test cases for the User API endpoints"""
+    """Test cases for the User Register API endpoint"""
 
     @pytest.fixture(autouse=True)
     def setup_teardown(self):
@@ -50,17 +50,14 @@ class TestRegister():
             called["count"] += 1
             called["data"] = data
             return {"false_key": "false_data"}, 200
-
         monkeypatch.setattr(ServiceUser, "register", fake_service_handler)
 
-        # No json= and no data=
         response = self.client.post('/api/user/register')
 
         assert response.status_code == 200
         assert response.is_json
         assert response.get_json() == {"false_key": "false_data"}
 
-        # Service should still be called once with empty dict
         assert called["count"] == 1
         assert called["data"] == {}
 
@@ -118,6 +115,121 @@ class TestRegister():
 
         response = self.client.delete(
             '/api/user/register',
+            json={"false_json_key": "false_json_data"}
+        )
+
+        assert response.status_code == 405
+        assert not response.is_json
+
+        assert called["count"] == 0
+        assert called["data"] == None
+
+
+class TestUsername():
+    """Test cases for the User Username API endpoint"""
+
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self):
+        self.app = Flask(__name__)
+        self.app.register_blueprint(user_bp)
+        self.client = self.app.test_client()
+
+    def test_call_frontend(self, monkeypatch):
+        """Should pass data through to frontend, and return given code"""
+
+        called = {"count": 0, "data": None}
+        def fake_service_handler(data: Dict[str, Any]):
+            called["count"] += 1
+            called["data"] = data
+            return {"false_key": "false_data"}, 546
+        monkeypatch.setattr(ServiceUser, "username", fake_service_handler)
+
+        response = self.client.post(
+            '/api/user/username',
+            json={"false_json_key": "false_json_data"}
+        )
+
+        assert response.status_code == 546
+        assert response.is_json
+        assert response.get_json() == {"false_key": "false_data"}
+
+        assert called["count"] == 1
+        assert called["data"] == {"false_json_key": "false_json_data"}
+
+    def test_no_json_content(self, monkeypatch):
+        """Should pass empty dict if no JSON or form data"""
+
+        called = {"count": 0, "data": None}
+
+        def fake_service_handler(data: Dict[str, Any]):
+            called["count"] += 1
+            called["data"] = data
+            return {"false_key": "false_data"}, 200
+        monkeypatch.setattr(ServiceUser, "username", fake_service_handler)
+
+        response = self.client.post('/api/user/username')
+
+        assert response.status_code == 200
+        assert response.is_json
+        assert response.get_json() == {"false_key": "false_data"}
+
+        assert called["count"] == 1
+        assert called["data"] == {}
+
+    def test_get_request(self, monkeypatch):
+        """Should fail if GET request used"""
+
+        called = {"count": 0, "data": None}
+        def fake_service_handler(data: Dict[str, Any]):
+            called["count"] += 1
+            called["data"] = data
+            return {"false_key": "false_data"}, 546
+        monkeypatch.setattr(ServiceUser, "username", fake_service_handler)
+
+        response = self.client.get(
+            '/api/user/username',
+            json={"false_json_key": "false_json_data"}
+        )
+
+        assert response.status_code == 405
+        assert not response.is_json
+
+        assert called["count"] == 0
+        assert called["data"] == None
+
+    def test_put_request(self, monkeypatch):
+        """Should fail if PUT request used"""
+
+        called = {"count": 0, "data": None}
+        def fake_service_handler(data: Dict[str, Any]):
+            called["count"] += 1
+            called["data"] = data
+            return {"false_key": "false_data"}, 546
+        monkeypatch.setattr(ServiceUser, "username", fake_service_handler)
+
+        response = self.client.put(
+            '/api/user/username',
+            json={"false_json_key": "false_json_data"}
+        )
+
+        assert response.status_code == 405
+        assert not response.is_json
+
+        assert called["count"] == 0
+        assert called["data"] == None
+
+    def test_delete_request(self, monkeypatch):
+        """Should fail if DELETE request used"""
+
+        called = {"count": 0, "data": None}
+        def fake_service_handler(data: Dict[str, Any]):
+            called["count"] += 1
+            called["data"] = data
+            return {"false_key": "false_data"}, 546
+        monkeypatch.setattr(ServiceUser, "username", fake_service_handler)
+
+        response = self.client.delete(
+            '/api/user/username',
             json={"false_json_key": "false_json_data"}
         )
 
