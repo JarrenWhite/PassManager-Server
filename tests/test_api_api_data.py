0b78714087fe52d5318cc8b4b2ef8ec643139fdb
Jarren White <phillrucci@hotmail.co.uk>
2025-12-06 17:25:20 +0000

    Setup test file for data api endpoints.

---
+++ tests/test_api_api_data.py
@@ -0,0 +1,15 @@
+import os
+import sys
+import pytest
+from flask import Flask
+from typing import Dict, Any
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from api.api_data import data_bp
+from services.service_data import ServiceData
+
+
+
+if __name__ == '__main__':
+    pytest.main(['-v', __file__])
