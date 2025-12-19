46ef19a747987dca12268ea3896359c57e7a20d2
Phill <phillrucci@hotmail.co.uk>
2025-12-19 19:30:15 +0000

    Test reesponse is correct.

--- tests/test_services_service_user.py
+++ tests/test_services_service_user.py
@@ -40,6 +40,31 @@ class TestRegister():
         assert len(fake_create_calls) == 1
         assert fake_create_calls[0] == ("fake_hash", "fake_srp_salt", "fake_srp_verifier", "fake_master_salt")
 
+    def test_generates_correct_response(self, monkeypatch):
+        """Should return correct response and http code"""
+
+        fake_create_calls = []
+        def fake_create(
+            username_hash: str,
+            srp_salt: str,
+            srp_verifier: str,
+            master_key_salt: str
+        ):
+            fake_create_calls.append((username_hash, srp_salt, srp_verifier, master_key_salt))
+            return True, None
+        monkeypatch.setattr(DBUtilsUser, "create", fake_create)
+
+        data = {
+            "username_hash": "fake_hash",
+            "srp_salt": "fake_srp_salt",
+            "srp_verifier": "fake_srp_verifier",
+            "master_key_salt": "fake_master_salt"
+        }
+        response, code = ServiceUser.register(data)
+
+        assert response == {"success": True, "username_hash": "fake_hash", "errors": []}
+        assert code == 201
+
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
