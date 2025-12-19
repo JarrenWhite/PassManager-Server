637b69c49e5e9b0004e14dbc4d67461a0c84ed19
Phill <phillrucci@hotmail.co.uk>
2025-12-19 19:23:11 +0000

    ServiceUser.create creates user

--- src/services/service_user.py
+++ src/services/service_user.py
@@ -1,10 +1,12 @@
 from typing import Tuple, Dict, Any
 
+from utils import DBUtilsUser
 
 class ServiceUser():
 
     @staticmethod
     def register(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
+        DBUtilsUser.create(data["username_hash"], data["srp_salt"], data["srp_verifier"], data["master_key_salt"])
         return {}, 200
 
     @staticmethod
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,2 +1,3 @@
 from .logging_setup import setup_logging
 from .database_config import DatabaseConfig
+from .db_utils_user import DBUtilsUser
