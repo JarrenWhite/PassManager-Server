5ce8aace0cb0a0c2e9b3d8e6893b7fb32566d242
Phill <phillrucci@hotmail.co.uk>
2025-12-19 19:30:38 +0000

    Create returns correct response if successful

--- src/services/service_user.py
+++ src/services/service_user.py
@@ -7,7 +7,8 @@ class ServiceUser():
     @staticmethod
     def register(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
         DBUtilsUser.create(data["username_hash"], data["srp_salt"], data["srp_verifier"], data["master_key_salt"])
-        return {}, 200
+
+        return {"success": True, "username_hash": data["username_hash"], "errors": []}, 201
 
     @staticmethod
     def username(data: Dict[str, Any]) -> Tuple[Dict[str, Any], int]:
