b7eb5bdb0ac477dcbc8c30bb6d25a6b8e45c37b1
Phill <phillrucci@hotmail.co.uk>
2025-12-20 10:48:28 +0000

    Create service_utils helper function for handling a failure reason.

--- src/utils/service_utils.py
+++ src/utils/service_utils.py
@@ -1,5 +1,6 @@
-from typing import Dict, Any, Tuple
+from typing import Dict, Any, Tuple, Optional
 
+from enums import FailureReason
 
 
 # TODO - Placeholder class. Requires completion.
@@ -8,6 +9,25 @@ class ServiceUtils():
     """Utility functions for managing service calls"""
 
     @staticmethod
-    def sanitise_inputs(data: Dict[str, Any], required_keys: set[str]) -> Tuple[bool, Dict[str, Any]]:
-        """Verify that the received keys are correct"""
-        return True, {}
+    def sanitise_inputs(data: Dict[str, Any], required_keys: set[str]) -> Tuple[bool, Dict[str, Any], Optional[int]]:
+        """
+        Verify that the received keys are correct
+
+        Returns:
+            (bool)  Returns true if sanitised, false otherwise
+            (Dict)  Error message if sanitisation fails
+            (int)   HTTP status code if sanitisation fails
+        """
+        return True, {}, None
+
+
+    @staticmethod
+    def handle_failure_reason(failure_reason: FailureReason) -> Tuple[Dict[str, Any], int]:
+        """
+        Verify that the
+
+        Returns:
+            (Dict)  Error message for failure reason
+            (int)   HTTP status code for failure reason
+        """
+        return {}, 200
