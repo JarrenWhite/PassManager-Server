8cc51e6141f3de56563d11491cfc003329de546c
Phill <phillrucci@hotmail.co.uk>
2026-01-06 19:09:11 +0000

    Create shell of function to seal session data

--- src/utils/session_manager.py
+++ src/utils/session_manager.py
@@ -19,6 +19,21 @@ class SessionManager():
             (bool)  Returns true if decrypted, false otherwise
             (Dict)  Decrypted values if successful, error message if failed
             (int)   Internal User ID if successful
-            (int)   HTTP status code if session decoding fails
+            (int)   HTTP status code if session decrypting fails
         """
         return True, {}, 0, 0
+
+    @staticmethod
+    def seal_session(
+        session_id: str,
+        unencrypted_data: Dict[str, Any]
+    ) -> Tuple[bool, str, int]:
+        """
+        Docstring for seal_session
+
+        Returns:
+            (bool)  Returns true if encrypted, false otherwise
+            (Dict)  Encrypted data if successful
+            (int)   HTTP status code if session encrypting fails
+        """
+        return True, "", 0
