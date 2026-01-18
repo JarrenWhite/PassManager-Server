22c493e0be8a7b7262ee00f68a06c2d001ddcb80
Phill <phillrucci@hotmail.co.uk>
2026-01-18 10:56:43 +0000

    Adjust SRP utils to use bytes over str.

--- src/cryptography/srp_utils.py
+++ src/cryptography/srp_utils.py
@@ -7,44 +7,45 @@ class SRPUtils():
 
     @staticmethod
     def generate_ephemeral(
-        srp_verifier_v: str
-    ) -> Tuple[str, str]:
+        srp_verifier_v: bytes
+    ) -> Tuple[bytes, bytes]:
         """
         Generate the ephemeral values
 
         Returns:
-            (str)   Public Ephemeral (B)
-            (str)   Private Ephemeral (b)
+            (bytes) Public Ephemeral (B)
+            (bytes) Private Ephemeral (b)
         """
-        return "", ""
+        return b'', b''
 
 
     @staticmethod
     def compute_session_key(
-        eph_val_A: str,
-        eph_public_B: str,
-        eph_private_b: str,
-        srp_verifier_v: str
-    ) -> str:
+        eph_val_A: bytes,
+        eph_public_B: bytes,
+        eph_private_b: bytes,
+        srp_verifier_v: bytes
+    ) -> bytes:
         """
         Compute the session key from its values
 
         Returns:
-            (str)   The session key (K)
+            (bytes) The session key (K)
         """
-        return ""
+        return b''
+
 
     @staticmethod
     def verify_proof(
-        eph_val_A: str,
-        eph_public_B: str,
-        session_key_K: str,
-        proof_val_M1: str
-    ) -> str:
+        eph_val_A: bytes,
+        eph_public_B: bytes,
+        session_key_K: bytes,
+        proof_val_M1: bytes
+    ) -> bytes:
         """
         Verify the proof of a matching session key
 
         Returns:
-            Server Proof (M2)
+            (bytes) Server Proof (M2)
         """
-        return ""
+        return b''
