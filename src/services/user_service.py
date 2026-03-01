00917b7f08e073666e0197bfbda40df88da7356c
Phill <phillrucci@hotmail.co.uk>
2026-03-01 21:29:00 +0000

    Connect service to service handlers for user

--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,16 +1,18 @@
 from passmanager.user.v0.user_pb2_grpc import UserServicer
 
+from user_handler import UserHandler
+
 
 class UserService(UserServicer):
 
     def Register(self, request, context):
-        return super().Register(request, context)
+        return UserHandler.register(request)
 
     def Username(self, request, context):
-        return super().Username(request, context)
+        return UserHandler.username(request)
 
     def Delete(self, request, context):
-        return super().Delete(request, context)
+        return UserHandler.delete(request)
 
     def Health(self, request, context):
         return super().Health(request, context)
