135ac6933db215ae6ef7c3bb827031907af25f75
Phill <phillrucci@hotmail.co.uk>
2026-03-11 23:41:13 +0000

    Add logging to password service

--- src/services/password_service.py
+++ src/services/password_service.py
@@ -1,3 +1,6 @@
+from logging import getLogger
+logger = getLogger("api")
+
 from passmanager.password.v0.password_pb2_grpc import PasswordServicer
 from passmanager.common.v0.error_pb2 import (
     HealthResponse
@@ -9,22 +12,29 @@ from password_handler import PasswordHandler
 class PasswordService(PasswordServicer):
 
     def Start(self, request, context):
+        logger.info("Start called by: %s", context.peer())
         return PasswordHandler.start(request)
 
     def Auth(self, request, context):
+        logger.info("Auth called by: %s", context.peer())
         return PasswordHandler.auth(request)
 
     def Complete(self, request, context):
+        logger.info("Complete called by: %s", context.peer())
         return PasswordHandler.complete(request)
 
     def Abort(self, request, context):
+        logger.info("Abort called by: %s", context.peer())
         return PasswordHandler.abort(request)
 
     def Get(self, request, context):
+        logger.info("Get called by: %s", context.peer())
         return PasswordHandler.get(request)
 
     def Update(self, request, context):
+        logger.info("Update called by: %s", context.peer())
         return PasswordHandler.update(request)
 
     def Health(self, request, context):
+        logger.info("Health called by: %s", context.peer())
         return HealthResponse(health=True)
