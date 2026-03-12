acd85d39c63516b75c3e7f550cec85570d995501
Phill <phillrucci@hotmail.co.uk>
2026-03-12 16:38:04 +0000

    Add logging to session service.

--- src/services/session_service.py
+++ src/services/session_service.py
@@ -1,3 +1,6 @@
+from logging import getLogger
+logger = getLogger("api")
+
 from passmanager.session.v0.session_pb2_grpc import SessionServicer
 from passmanager.common.v0.error_pb2 import (
     HealthResponse
@@ -9,16 +12,21 @@ from session_handler import SessionHandler
 class SessionService(SessionServicer):
 
     def Start(self, request, context):
+        logger.info("Start called by: %s", context.peer())
         return SessionHandler.start(request)
 
     def Auth(self, request, context):
+        logger.info("Auth called by: %s", context.peer())
         return SessionHandler.auth(request)
 
     def Delete(self, request, context):
+        logger.info("Delete called by: %s", context.peer())
         return SessionHandler.delete(request)
 
     def Clean(self, request, context):
+        logger.info("Clean called by: %s", context.peer())
         return SessionHandler.clean(request)
 
     def Health(self, request, context):
+        logger.info("Health called by: %s", context.peer())
         return HealthResponse(health=True)
