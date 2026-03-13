335229159930e0b35b63e6a9a2e3699824d58674
Phill <phillrucci@hotmail.co.uk>
2026-03-13 16:27:43 +0000

    Add logging to data service.

--- src/services/data_service.py
+++ src/services/data_service.py
@@ -1,3 +1,6 @@
+from logging import getLogger
+logger = getLogger("api")
+
 from passmanager.data.v0.data_pb2_grpc import DataServicer
 from passmanager.common.v0.error_pb2 import (
     HealthResponse
@@ -9,19 +12,25 @@ from data_handler import DataHandler
 class DataService(DataServicer):
 
     def Create(self, request, context):
+        logger.info("Create called by: %s", context.peer())
         return DataHandler.create(request)
 
     def Edit(self, request, context):
+        logger.info("Edit called by: %s", context.peer())
         return DataHandler.edit(request)
 
     def Delete(self, request, context):
+        logger.info("Delete called by: %s", context.peer())
         return DataHandler.delete(request)
 
     def Get(self, request, context):
+        logger.info("Get called by: %s", context.peer())
         return DataHandler.get(request)
 
     def List(self, request, context):
+        logger.info("List called by: %s", context.peer())
         return DataHandler.list(request)
 
     def Health(self, request, context):
+        logger.info("Health called by: %s", context.peer())
         return HealthResponse(health=True)
