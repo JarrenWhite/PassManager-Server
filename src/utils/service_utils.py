43b8f642888df8045b3a959dc5dad7d712dcd52a
Phill <phillrucci@hotmail.co.uk>
2026-03-22 21:17:38 +0000

    Create outline for service utils sanitising inputs for user register.

---
+++ src/utils/service_utils.py
@@ -0,0 +1,19 @@
+
+
+class ServiceUtils():
+
+    @staticmethod
+    def sanitise_username():
+        pass
+
+    @staticmethod
+    def sanitise_srp_salt():
+        pass
+
+    @staticmethod
+    def sanitise_srp_verifier():
+        pass
+
+    @staticmethod
+    def sanitise_master_key_salt():
+        pass
