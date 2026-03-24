8af9e7fdad74e80d618f1505d2bb3100bac6bac1
Phill <phillrucci@hotmail.co.uk>
2026-03-24 19:26:43 +0000

    Add full signatures for service utils.

--- src/utils/service_utils.py
+++ src/utils/service_utils.py
@@ -1,19 +1,34 @@
+from typing import Tuple, Optional
+
+from enums import ServiceError
 
 
 class ServiceUtils():
 
-    @staticmethod
-    def sanitise_username():
-        pass
 
     @staticmethod
-    def sanitise_srp_salt():
-        pass
+    def sanitise_username(
+        input: bytes
+    ) -> Tuple[bool, Optional[ServiceError]]:
+        return True, None
+
 
     @staticmethod
-    def sanitise_srp_verifier():
-        pass
+    def sanitise_srp_salt(
+        input: bytes
+    ) -> Tuple[bool, Optional[ServiceError]]:
+        return True, None
+
 
     @staticmethod
-    def sanitise_master_key_salt():
-        pass
+    def sanitise_srp_verifier(
+        input: bytes
+    ) -> Tuple[bool, Optional[ServiceError]]:
+        return True, None
+
+
+    @staticmethod
+    def sanitise_master_key_salt(
+        input: bytes
+    ) -> Tuple[bool, Optional[ServiceError]]:
+        return True, None
