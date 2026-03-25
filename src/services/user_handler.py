fdd045b608705e7e1f25014f9bb7b0081d5ca2e8
Phill <phillrucci@hotmail.co.uk>
2026-03-25 17:30:31 +0000

    Pass master key salt test.

--- src/services/user_handler.py
+++ src/services/user_handler.py
@@ -21,6 +21,7 @@ class UserHandler():
         ServiceUtils.sanitise_username(request.new_username)
         ServiceUtils.sanitise_srp_salt(request.srp_salt)
         ServiceUtils.sanitise_srp_verifier(request.srp_verifier)
+        ServiceUtils.sanitise_master_key_salt(request.master_key_salt)
 
         return UserRegisterResponse()
 
