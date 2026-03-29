55a6ccb92ce4a799274678ef2d8cfa6cd7422854
Phill <phillrucci@hotmail.co.uk>
2026-03-29 16:09:01 +0100

    Create dummy method to open session.

---
+++ src/utils/session_manager.py
@@ -0,0 +1,15 @@
+from typing import Tuple, Optional
+
+from passmanager.common.v0.secure_pb2 import (
+    SecureRequest,
+    SecureResponse
+)
+
+from enums import FailureReason
+
+
+class SessionManager():
+
+    @staticmethod
+    def open_session(request: SecureRequest) -> Tuple[bool, bytes, Optional[FailureReason]]:
+        return True, b'', None
