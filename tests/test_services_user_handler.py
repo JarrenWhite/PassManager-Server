809539bb5e3c7b827cc871407626554277785a0e
Phill <phillrucci@hotmail.co.uk>
2026-03-29 17:40:59 +0100

    Test open session failing

--- tests/test_services_user_handler.py
+++ tests/test_services_user_handler.py
@@ -149,6 +149,7 @@ class TestRegister:
 
         response = UserHandler.register(request)
 
+        assert isinstance(response, UserRegisterResponse)
         assert not response.success
         assert len(response.failure_data.error_list) == 1
 
@@ -278,7 +279,7 @@ class TestUsername:
     def setup_teardown(self, monkeypatch):
 
         self.open_session_called = []
-        self.open_session_response = True, b'', None
+        self.open_session_response = True, b'fake_decrypted_bytes', None
         def fake_open_session(request):
             self.open_session_called.append(request)
             return self.open_session_response
@@ -299,6 +300,28 @@ class TestUsername:
 
         assert len(self.open_session_called) == 1
         assert self.open_session_called[0] == request
+
+    def test_open_session_fails(self):
+        """Should return error if open session fails"""
+
+        self.open_session_response = False, b'', FailureReason.DECRYPTION
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = UserHandler.username(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == "request"
+        assert error.code == ErrorCode.RQS01
+        assert error.description == FailureReason.INVALID.description
 
 
 if __name__ == '__main__':
