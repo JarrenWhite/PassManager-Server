c2af2a81523025c96f6b301a2b302dad379cae74
Phill <phillrucci@hotmail.co.uk>
2026-03-31 21:12:36 +0100

    Test sanitising fails for user delete

--- tests/test_services_user_handler.py
+++ tests/test_services_user_handler.py
@@ -776,6 +776,26 @@ class TestDelete():
         assert len(self.sanitise_username_called) == 1
         assert self.sanitise_username_called[0] == b'fake_username_hash'
 
+    def test_all_sanitising_functions_fail(self):
+        """Should fetch all missing errors if all sanitising fails"""
+
+        self.sanitise_username_response = FailureReason.INVALID
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = UserHandler.delete(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        fields = [error.field for error in response.failure_data.error_list]
+        assert "username_hash" in fields
+
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
