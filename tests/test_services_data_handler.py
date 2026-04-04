009e2ef088d9a2e7320b027edcf71ca3a33a91a1
JarrenWhite <phillrucci@hotmail.co.uk>
2026-04-04 10:36:04 +0100

    Test converts to proto

--- tests/test_services_data_handler.py
+++ tests/test_services_data_handler.py
@@ -46,6 +46,19 @@ class TestCreate:
             return self.open_session_response
         monkeypatch.setattr(SessionManager, "open_session", fake_open_session)
 
+        self.from_string_called = []
+        self.from_string_response = DataCreateRequest(
+            username_hash=b'fake_username_hash'
+        )
+        self.from_string_exception = False
+        def fake_from_string(data):
+            self.from_string_called.append(data)
+            if self.from_string_exception:
+                raise DecodeError("invalid bytes")
+            else:
+                return self.from_string_response
+        monkeypatch.setattr(DataCreateRequest, "FromString", fake_from_string)
+
         yield
 
     def test_calls_open_session(self):
@@ -83,6 +96,22 @@ class TestCreate:
         assert error.field == "request"
         assert error.code == ErrorCode.RQS01
         assert error.description == FailureReason.DECRYPTION.description
+
+    def test_calls_to_convert_to_proto(self):
+        """Should attempt to convert returned bytes to protobuf"""
+
+        self.from_string_response = DataCreateRequest()
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.create(request)
+
+        assert len(self.from_string_called) == 1
+        assert self.from_string_called[0] == b'fake_decrypted_bytes'
 
 
 if __name__ == '__main__':
