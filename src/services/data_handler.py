319a9d5dbfc4278e0dad01b8a98910f9af9bd818
Phill <phillrucci@hotmail.co.uk>
2026-04-09 19:57:13 +0100

    Convert to protobuf message

--- src/services/data_handler.py
+++ src/services/data_handler.py
@@ -136,6 +136,20 @@ class DataHandler:
                 failure_data=failure
             )
 
+        # Convert to Protobuf Message
+        try:
+            request = DataEditRequest.FromString(decrypted_bytes)
+        except DecodeError:
+            error_list.append(FailureReason.DECRYPTION.error_proto())
+
+            failure = Failure(
+                error_list=error_list
+            )
+            return SecureResponse(
+                success=False,
+                failure_data=failure
+            )
+
 
         return SecureResponse()
 
