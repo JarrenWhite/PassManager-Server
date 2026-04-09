ec22cc4d1404b91fd612833876fc39adb483ede6
Phill <phillrucci@hotmail.co.uk>
2026-04-09 20:17:22 +0100

    Test for sanitising calls for each input

--- tests/test_services_data_handler.py
+++ tests/test_services_data_handler.py
@@ -475,6 +475,34 @@ class TestEdit:
                 return self.from_string_response
         monkeypatch.setattr(DataEditRequest, "FromString", fake_from_string)
 
+        self.sanitise_username_called = []
+        self.sanitise_username_response = None
+        def fake_sanitise_username(input):
+            self.sanitise_username_called.append(input)
+            return self.sanitise_username_response
+        monkeypatch.setattr(ServiceUtils, "sanitise_username", fake_sanitise_username)
+
+        self.sanitise_entry_public_id_called = []
+        self.sanitise_entry_public_id_response = None
+        def fake_sanitise_entry_public_id(input):
+            self.sanitise_entry_public_id_called.append(input)
+            return self.sanitise_entry_public_id_response
+        monkeypatch.setattr(ServiceUtils, "sanitise_entry_public_id", fake_sanitise_entry_public_id)
+
+        self.sanitise_entry_name_called = []
+        self.sanitise_entry_name_response = None
+        def fake_sanitise_entry_name(input):
+            self.sanitise_entry_name_called.append(input)
+            return self.sanitise_entry_name_response
+        monkeypatch.setattr(ServiceUtils, "sanitise_entry_name", fake_sanitise_entry_name)
+
+        self.sanitise_entry_data_called = []
+        self.sanitise_entry_data_response = None
+        def fake_sanitise_entry_data(input):
+            self.sanitise_entry_data_called.append(input)
+            return self.sanitise_entry_data_response
+        monkeypatch.setattr(ServiceUtils, "sanitise_entry_data", fake_sanitise_entry_data)
+
         yield
 
     def test_calls_open_session(self):
@@ -550,6 +578,70 @@ class TestEdit:
         assert error.field == "request"
         assert error.code == ErrorCode.RQS01
         assert error.description == FailureReason.DECRYPTION.description
+
+    def test_calls_sanitise_username(self):
+        """Should call sanitise username"""
+
+        self.from_string_response.username_hash = b'fake_username_hash'
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert len(self.sanitise_username_called) == 1
+        assert self.sanitise_username_called[0] == b'fake_username_hash'
+
+    def test_calls_sanitise_entry_public_id(self):
+        """Should call sanitise entry public id"""
+
+        self.from_string_response.entry_public_id = "fake_entry_public_id"
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert len(self.sanitise_entry_public_id_called) == 1
+        assert self.sanitise_entry_public_id_called[0] == "fake_entry_public_id"
+
+    def test_calls_sanitise_entry_name(self):
+        """Should call sanitise entry name"""
+
+        self.from_string_response.entry_name = b'fake_entry_name'
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert len(self.sanitise_entry_name_called) == 1
+        assert self.sanitise_entry_name_called[0] == b'fake_entry_name'
+
+    def test_calls_sanitise_entry_data(self):
+        """Should call sanitise entry data"""
+
+        self.from_string_response.entry_data = b'fake_entry_data'
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert len(self.sanitise_entry_data_called) == 1
+        assert self.sanitise_entry_data_called[0] == b'fake_entry_data'
 
 
 if __name__ == '__main__':
