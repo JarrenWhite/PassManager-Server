46c57acf434463bfa25326259e0594c4ec734992
Phill <phillrucci@hotmail.co.uk>
2026-04-09 19:43:57 +0100

    Begin tests for data edit. Test open session

--- tests/test_services_data_handler.py
+++ tests/test_services_data_handler.py
@@ -446,5 +446,35 @@ class TestCreate:
         assert response == secure_response
 
 
+class TestEdit:
+    """Test cases for data edit function"""
+
+    @pytest.fixture(autouse=True)
+    def setup_teardown(self, monkeypatch):
+
+        self.open_session_called = []
+        self.open_session_response = True, b'fake_decrypted_bytes', 0, None
+        def fake_open_session(request, password_session = False, first_request = False):
+            self.open_session_called.append((request, password_session, first_request))
+            return self.open_session_response
+        monkeypatch.setattr(SessionManager, "open_session", fake_open_session)
+
+        yield
+
+    def test_calls_open_session(self):
+        """Should pass secure request to be opened"""
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert len(self.open_session_called) == 1
+        assert self.open_session_called[0] == (request, True, True)
+
+
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
