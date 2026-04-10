c5210b5e04c0f552091592b9a11de1fa2d598338
Phill <phillrucci@hotmail.co.uk>
2026-04-10 18:51:48 +0100

    Test sanitising failures

--- tests/test_services_data_handler.py
+++ tests/test_services_data_handler.py
@@ -673,9 +673,62 @@ class TestEdit:
 
         assert len(self.sanitise_entry_data_called) == 0
 
+    @pytest.mark.parametrize(
+        "failing_sanitiser, field",
+        [
+            ("sanitise_username",           "username_hash"),
+            ("sanitise_entry_public_id",    "entry_public_id"),
+            ("sanitise_entry_name",         "entry_name"),
+            ("sanitise_entry_data",         "entry_data")
+        ]
+    )
+    def test_each_sanitising_invalid_failure(self, failing_sanitiser, field):
+        """Should fetch invalid error for each sanitation fail"""
 
+        setattr(self, f"{failing_sanitiser}_response", FailureReason.INVALID)
 
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
 
+        response = DataHandler.edit(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == field
+        assert error.code == ErrorCode.GNR00
+        assert error.description == FailureReason.INVALID.description
+
+    def test_all_sanitising_functions_fail(self):
+        """Should fetch all missing errors if all sanitising fails"""
+
+        self.sanitise_username_response = FailureReason.INVALID
+        self.sanitise_entry_public_id_response = FailureReason.INVALID
+        self.sanitise_entry_name_response = FailureReason.INVALID
+        self.sanitise_entry_data_response = FailureReason.INVALID
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.edit(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 4
+
+        fields = [error.field for error in response.failure_data.error_list]
+        assert "username_hash" in fields
+        assert "entry_public_id" in fields
+        assert "entry_name" in fields
+        assert "entry_data" in fields
 
 
 if __name__ == '__main__':
