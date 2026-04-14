f760a64345a1e0b61c248b32ce49349c858c85d4
Phill <phillrucci@hotmail.co.uk>
2026-04-14 22:49:17 +0100

    Call proto

--- src/services/data_handler.py
+++ src/services/data_handler.py
@@ -311,6 +311,20 @@ class DataHandler:
                 failure_data=failure
             )
 
+        # Convert to Protobuf Message
+        try:
+            request = DataGetRequest.FromString(decrypted_bytes)
+        except DecodeError:
+            error_list.append(FailureReason.DECRYPTION.error_proto())
+
+            failure = Failure(
+                error_list=error_list
+            )
+            return SecureResponse(
+                success=False,
+                failure_data=failure
+            )
+
 
         return SecureResponse()
 
