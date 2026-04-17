b742387026da555eb84f27f77fe942104bf69fdf
Phill <phillrucci@hotmail.co.uk>
2026-04-17 18:03:14 +0100

    Test for sanitising failures

--- tests/test_services_data_handler.py
+++ tests/test_services_data_handler.py
@@ -1514,6 +1514,57 @@ class TestGet:
         assert len(self.sanitise_entry_public_id_called) == 1
         assert self.sanitise_entry_public_id_called[0] == "fake_entry_public_id"
 
+    @pytest.mark.parametrize(
+        "failing_sanitiser, field",
+        [
+            ("sanitise_username",           "username_hash"),
+            ("sanitise_entry_public_id",    "entry_public_id")
+        ]
+    )
+    def test_each_sanitising_invalid_failure(self, failing_sanitiser, field):
+        """Should fetch invalid error for each sanitation fail"""
+
+        setattr(self, f"{failing_sanitiser}_response", FailureReason.INVALID)
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.get(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == field
+        assert error.code == ErrorCode.GNR00
+        assert error.description == FailureReason.INVALID.description
+
+    def test_all_sanitising_functions_fail(self):
+        """Should fetch all missing errors if all sanitising fails"""
+
+        self.sanitise_username_response = FailureReason.INVALID
+        self.sanitise_entry_public_id_response = FailureReason.INVALID
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = DataHandler.get(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 2
+
+        fields = [error.field for error in response.failure_data.error_list]
+        assert "username_hash" in fields
+        assert "entry_public_id" in fields
+
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
