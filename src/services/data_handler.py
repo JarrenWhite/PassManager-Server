259733ad6ef6838d940eb102f676a8e7b4da3615
Phill <phillrucci@hotmail.co.uk>
2026-04-20 11:28:27 +0100

    Pass initial tests for data list function

--- src/services/data_handler.py
+++ src/services/data_handler.py
@@ -377,4 +377,24 @@ class DataHandler:
 
     @staticmethod
     def list(secure_request: SecureRequest) -> SecureResponse:
+        error_list = []
+
+        # Open secure session
+        open_session = SessionManager.open_session(
+            request=secure_request
+        )
+        status, decrypted_bytes, user_id, failure_reason = open_session
+        if not status:
+            assert failure_reason
+            error_list.append(failure_reason.error_proto())
+
+            failure = Failure(
+                error_list=error_list
+            )
+            return SecureResponse(
+                success=False,
+                failure_data=failure
+            )
+
+
         return SecureResponse()
