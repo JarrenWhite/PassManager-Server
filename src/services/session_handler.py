ef5a72e11ab2d2003dfbdfb30dcbfdba8c91f914
Phill <phillrucci@hotmail.co.uk>
2026-05-10 21:04:04 +0100

    Convert to proto and return if failure

--- src/services/session_handler.py
+++ src/services/session_handler.py
@@ -170,6 +170,20 @@ class SessionHandler:
                 failure_data=failure
             )
 
+        # Convert to Protobuf Message
+        try:
+            request = SessionDeleteRequest.FromString(decrypted_bytes)
+        except DecodeError:
+            error_list.append(FailureReason.DECRYPTION.error_proto())
+
+            failure = Failure(
+                error_list=error_list
+            )
+            return SecureResponse(
+                success=False,
+                failure_data=failure
+            )
+
 
 
         return SecureResponse()
