a0e52a710535727ac05db87ed61b503c3045ea3b
Phill <phillrucci@hotmail.co.uk>
2026-05-21 11:21:10 +0100

    Test password sanitising fails

--- tests/test_services_password_handler.py
+++ tests/test_services_password_handler.py
@@ -232,7 +232,62 @@ class TestStart():
         assert len(self.sanitise_master_key_salt_called) == 1
         assert self.sanitise_master_key_salt_called[0] == b'fake_master_key_salt'
 
+    @pytest.mark.parametrize(
+        "failing_sanitiser, field",
+        [
+            ("sanitise_username",           "username_hash"),
+            ("sanitise_srp_salt",           "srp_salt"),
+            ("sanitise_srp_verifier",       "srp_verifier"),
+            ("sanitise_master_key_salt",    "master_key_salt")
+        ]
+    )
+    def test_each_sanitising_invalid_failure(self, failing_sanitiser, field):
+        """Should fetch invalid error for each sanitation fail"""
 
+        setattr(self, f"{failing_sanitiser}_response", FailureReason.INVALID)
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.start(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == field
+        assert error.code == ErrorCode.GNR00
+        assert error.description == FailureReason.INVALID.description
+
+    def test_all_sanitising_functions_fail(self):
+        """Should fetch all missing errors if all sanitising fails"""
+
+        self.sanitise_username_response = FailureReason.INVALID
+        self.sanitise_srp_salt_response = FailureReason.INVALID
+        self.sanitise_srp_verifier_response = FailureReason.INVALID
+        self.sanitise_master_key_salt_response = FailureReason.INVALID
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.start(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 4
+
+        fields = [error.field for error in response.failure_data.error_list]
+        assert "username_hash" in fields
+        assert "srp_salt" in fields
+        assert "srp_verifier" in fields
+        assert "master_key_salt" in fields
 
 
 
