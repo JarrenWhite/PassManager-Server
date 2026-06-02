74d92b437ad52d0263e68449d3b8a7e9986a9774
Phill <phillrucci@hotmail.co.uk>
2026-06-02 21:28:36 +0100

    Test convert to proto

--- tests/test_services_password_handler.py
+++ tests/test_services_password_handler.py
@@ -1300,6 +1300,19 @@ class TestAbort():
             return self.open_session_response
         monkeypatch.setattr(SessionManager, "open_session", fake_open_session)
 
+        self.from_string_called = []
+        self.from_string_response = PasswordAbortRequest(
+            username_hash=b'fake_username_hash'
+        )
+        self.from_string_exception = False
+        def fake_from_string(data):
+            self.from_string_called.append(data)
+            if self.from_string_exception:
+                raise DecodeError("invalid bytes")
+            else:
+                return self.from_string_response
+        monkeypatch.setattr(PasswordAbortRequest, "FromString", fake_from_string)
+
         yield
 
     def test_calls_open_session(self):
@@ -1338,6 +1351,43 @@ class TestAbort():
         assert error.code == ErrorCode.RQS01
         assert error.description == FailureReason.DECRYPTION.description
 
+    def test_calls_to_convert_to_proto(self):
+        """Should attempt to convert returned bytes to protobuf"""
+
+        self.from_string_response = PasswordAuthRequest()
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.abort(request)
+
+        assert len(self.from_string_called) == 1
+        assert self.from_string_called[0] == b'fake_decrypted_bytes'
+
+    def test_convert_to_proto_fails(self):
+        """Should fail if conversion to proto raises exception"""
+
+        self.from_string_exception = True
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.abort(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == "request"
+        assert error.code == ErrorCode.RQS01
+        assert error.description == FailureReason.DECRYPTION.description
 
 
 if __name__ == '__main__':
