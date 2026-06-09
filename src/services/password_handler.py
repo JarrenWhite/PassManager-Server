ab60de8b7cff90bfda5b8eae044b9f81c1987f6e
Phill <phillrucci@hotmail.co.uk>
2026-06-09 20:42:14 +0100

    Handle open session fails

--- src/services/password_handler.py
+++ src/services/password_handler.py
@@ -468,6 +468,17 @@ class PasswordHandler():
             request=secure_request
         )
         status, failure_reason, decrypted_bytes, user_id = open_session
+        if not status:
+            assert failure_reason
+            error_list.append(failure_reason.error_proto())
+
+            failure = Failure(
+                error_list=error_list
+            )
+            return SecureResponse(
+                success=False,
+                failure_data=failure
+            )
 
 
 
