db1cdbb2ef127ca7ed765736bfe709cd62cbe609
Phill <phillrucci@hotmail.co.uk>
2026-06-13 13:10:50 +0100

    Test full sanitising

--- tests/test_services_password_handler.py
+++ tests/test_services_password_handler.py
@@ -2227,6 +2227,61 @@ class TestUpdate():
         assert len(self.sanitise_entry_data_called) == 1
         assert self.sanitise_entry_data_called[0] == b'fake_entry_data'
 
+    @pytest.mark.parametrize(
+        "failing_sanitiser, field",
+        [
+            ("sanitise_username",           "username_hash"),
+            ("sanitise_public_id",          "public_id"),
+            ("sanitise_entry_name",         "entry_name"),
+            ("sanitise_entry_data",         "entry_data")
+        ]
+    )
+    def test_each_sanitising_invalid_failure(self, failing_sanitiser, field):
+        """Should fetch invalid error for each sanitation fail"""
+
+        setattr(self, f"{failing_sanitiser}_response", FailureReason.INVALID)
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.update(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 1
+
+        error = response.failure_data.error_list[0]
+        assert error.field == field
+        assert error.code == ErrorCode.GNR00
+        assert error.description == FailureReason.INVALID.description
+
+    def test_all_sanitising_functions_fail(self):
+        """Should fetch all missing errors if all sanitising fails"""
+
+        self.sanitise_username_response = FailureReason.INVALID
+        self.sanitise_public_id_response = FailureReason.INVALID
+        self.sanitise_entry_name_response = FailureReason.INVALID
+        self.sanitise_entry_data_response = FailureReason.INVALID
+
+        request = SecureRequest(
+            session_id="fake_session_id",
+            request_number=0,
+            encrypted_data=b'fake_encryption_data'
+        )
+
+        response = PasswordHandler.update(request)
+
+        assert isinstance(response, SecureResponse)
+        assert not response.success
+        assert len(response.failure_data.error_list) == 4
+
+        fields = [error.field for error in response.failure_data.error_list]
+        assert "username_hash" in fields
+        assert "public_id" in fields
+
 
 if __name__ == '__main__':
     pytest.main(['-v', __file__])
