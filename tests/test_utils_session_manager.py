07e9db224d20fb34ad04d3a6d324394c677b7f0d
Phill <phillrucci@hotmail.co.uk>
2026-07-01 14:51:43 +0100

    Test calls to create new DB entry

--- tests/test_utils_session_manager.py
+++ tests/test_utils_session_manager.py
@@ -1,9 +1,11 @@
 import os
 import sys
 import pytest
+import datetime
 
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "src"))
 
+import utils.session_manager
 from utils.session_manager import SessionManager
 from utils.db_utils_auth import DBUtilsAuth
 from enums.failure_reason import FailureReason
@@ -29,6 +31,20 @@ class TestStartNewSession():
             self.generate_ephemeral_called.append(srp_verifier_v)
             return self.generate_ephemeral_response
         monkeypatch.setattr(SRPUtils, "generate_ephemeral", fake_generate_ephemeral)
+
+        self.now_response = datetime.datetime(2024, 1, 15, 12, 0, 0)
+        class FakeDatetime(datetime.datetime):
+            @classmethod
+            def now(cls, tz=None):
+                return self.now_response
+        monkeypatch.setattr(utils.session_manager, "datetime", FakeDatetime)
+
+        self.start_called = []
+        self.start_response = True, None, "fake_public_id"
+        def fake_start(user_id, eph_private_b, eph_public_b, expiry_time):
+            self.start_called.append((user_id, eph_private_b, eph_public_b, expiry_time))
+            return self.start_response
+        monkeypatch.setattr(DBUtilsAuth, "start", fake_start)
 
         yield
 
@@ -65,6 +81,7 @@ class TestStartNewSession():
 
         assert not result[0]
         assert result[1] == failure_reason
+        assert len(self.start_called) == 0
 
     @pytest.mark.parametrize(
         "srp_verifier",
@@ -83,6 +100,49 @@ class TestStartNewSession():
 
         assert len(self.generate_ephemeral_called) == 1
         assert self.generate_ephemeral_called[0] == srp_verifier
+
+    @pytest.mark.parametrize(
+        "user_id, eph_private_b, eph_public_b",
+        [
+            (0,     b'abc',     b'def'),
+            (15,    b'',        b''),
+            (350,   b'qcd'*100, b'ghi'*300)
+        ]
+    )
+    def test_create_db_entry(self, user_id, eph_private_b, eph_public_b):
+        """Should create entry in database with correct values"""
+
+        self.fetch_response = True, None, user_id, b'fake_srp_salt', b'fake_srp_verifier'
+        self.generate_ephemeral_response = eph_public_b, eph_private_b
+
+        result = SessionManager.start_new_session(b'fake_username_hash')
+
+        assert len(self.start_called) == 1
+
+        start = self.start_called[0]
+        assert start[0] == user_id
+        assert start[1] == eph_private_b
+        assert start[2] == eph_public_b
+
+    @pytest.mark.parametrize(
+        "now",
+        [
+            datetime.datetime(2024, 1, 15, 12, 0, 0),
+            datetime.datetime(2000, 6, 1, 0, 0, 0),
+            datetime.datetime(1999, 12, 31, 23, 59, 59)
+        ]
+    )
+    def test_sets_correct_expiry(self, now):
+        """Should create entry in database with correct expiry"""
+
+        self.now_response = now
+
+        result = SessionManager.start_new_session(b'fake_username_hash')
+
+        assert len(self.start_called) == 1
+        start = self.start_called[0]
+
+        assert start[3] == self.now_response + datetime.timedelta(seconds=180)
 
 
 if __name__ == '__main__':
