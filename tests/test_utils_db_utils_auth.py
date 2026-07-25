7dba93bea2a9844fc3d3f4d5e4f9c3e17f4eb024
Phill <phillrucci@hotmail.co.uk>
2026-07-25 10:16:18 +0100

    Update get details tests for new changes

--- tests/test_utils_db_utils_auth.py
+++ tests/test_utils_db_utils_auth.py
@@ -616,7 +616,60 @@ class TestGetDetails():
         assert called["user"] == fake_user
 
     def test_user_id_match_fails(self, monkeypatch):
-        """Should fail if user id does not match"""
+            """Should fail if user id does not match"""
+            mock_session = _MockSession()
+
+            @contextmanager
+            def mock_get_db_session():
+                try:
+                    yield mock_session
+                    mock_session.commit()
+                except Exception:
+                    mock_session.rollback()
+                    raise
+                finally:
+                    mock_session.close()
+            monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+            fake_user = User(
+                id=123456,
+                username_hash=b'fake_hash',
+                srp_salt=b'fake_srp_salt',
+                srp_verifier=b'fake_srp_verifier',
+                master_key_salt=b'fake_master_key_salt',
+                password_change=False
+            )
+
+            expiry = datetime.now() + timedelta(hours=1)
+            fake_ephemeral = AuthEphemeral(
+                id=123456,
+                user=fake_user,
+                public_id="ephemeral_fake_public_id",
+                eph_private_b=b'fake_eph_private_b',
+                eph_public_b=b'fake_eph_public_b',
+                expiry_time=expiry,
+                password_change=False
+            )
+
+            mock_query = _MockQuery([fake_ephemeral])
+            def fake_query(self, model):
+                return mock_query
+            monkeypatch.setattr(_MockSession, "query", fake_query)
+
+            response = DBUtilsAuth.get_details(
+                user_id=999999,
+                public_id="fake_public_id"
+            )
+
+            assert isinstance(response, tuple)
+            assert isinstance(response[0], bool)
+            assert isinstance(response[1], FailureReason)
+            assert len(mock_session._deletes) == 0
+            assert response[0] == False
+            assert response[1] == FailureReason.NOT_FOUND
+
+    def test_username_hash_match_fails(self, monkeypatch):
+        """Should fail if username hash does not match"""
         mock_session = _MockSession()
 
         @contextmanager
@@ -667,6 +720,58 @@ class TestGetDetails():
         assert len(mock_session._deletes) == 0
         assert response[0] == False
         assert response[1] == FailureReason.NOT_FOUND
+
+    def test_fails_for_no_id(self, monkeypatch):
+        """Should fail if no user id or username hash given"""
+        mock_session = _MockSession()
+
+        @contextmanager
+        def mock_get_db_session():
+            try:
+                yield mock_session
+                mock_session.commit()
+            except Exception:
+                mock_session.rollback()
+                raise
+            finally:
+                mock_session.close()
+        monkeypatch.setattr(DatabaseSetup, "get_db_session", mock_get_db_session)
+
+        fake_user = User(
+            id=123456,
+            username_hash=b'fake_hash',
+            srp_salt=b'fake_srp_salt',
+            srp_verifier=b'fake_srp_verifier',
+            master_key_salt=b'fake_master_key_salt',
+            password_change=False
+        )
+
+        expiry = datetime.now() + timedelta(hours=1)
+        fake_ephemeral = AuthEphemeral(
+            id=123456,
+            user=fake_user,
+            public_id="ephemeral_fake_public_id",
+            eph_private_b=b'fake_eph_private_b',
+            eph_public_b=b'fake_eph_public_b',
+            expiry_time=expiry,
+            password_change=False
+        )
+
+        mock_query = _MockQuery([fake_ephemeral])
+        def fake_query(self, model):
+            return mock_query
+        monkeypatch.setattr(_MockSession, "query", fake_query)
+
+        response = DBUtilsAuth.get_details(
+            public_id="fake_public_id"
+        )
+
+        assert isinstance(response, tuple)
+        assert isinstance(response[0], bool)
+        assert isinstance(response[1], FailureReason)
+        assert len(mock_session._deletes) == 0
+        assert response[0] == False
+        assert response[1] == FailureReason.SERVER_ERROR
 
 
 class TestComplete():
