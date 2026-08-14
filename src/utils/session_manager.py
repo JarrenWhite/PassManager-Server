02cdb52e5b27d17808cfc02894f6a752fb3f69a1
Phill <phillrucci@hotmail.co.uk>
2026-08-14 16:32:50 +0100

    Check for first request requirement

--- src/utils/session_manager.py
+++ src/utils/session_manager.py
@@ -273,6 +273,8 @@ class SessionManager():
         # Check Session type is correct
         if password_session != password_change:
             return False, [FailureReason.DECRYPTION.error_proto()], b'', 0
+        if first_request and request_count != 0:
+            return False, [FailureReason.DECRYPTION.error_proto()], b'', 0
 
         # Decrypt Request
         AESUtils.decrypt_request(
