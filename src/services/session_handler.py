96a104acc9049d260a9f669e827833cf3158683f
Phill <phillrucci@hotmail.co.uk>
2026-08-19 10:04:40 +0100

    Update call to error proto

--- src/services/session_handler.py
+++ src/services/session_handler.py
@@ -185,7 +185,7 @@ class SessionHandler:
             error_list.append(status.error_proto("username_hash"))
         status = ServiceUtils.sanitise_public_id(request.public_id)
         if status:
-            error_list.append(status.error_proto("session_id"))
+            error_list.append(status.error_proto("public_id"))
 
         # Return errors
         if len(error_list) > 0:
